f5d128045d16cb53daaaba7575b3b5a40fa2bb9e
Dhruv Sharma <dhruvninja@icloud.com>
2025-08-23 22:56:57 -0700

    a

--- cpp/list.cpp
+++ cpp/list.cpp
@@ -17,15 +17,30 @@ int main(int argc, char *argv[]) {
 
     while ((entry = readdir(dir)) != nullptr) {
         std::string fullPath = std::string(path) + "/" + entry->d_name;
-
+    
         if (stat(fullPath.c_str(), &fileStat) == -1) {
             perror("stat");
             continue;
         }
-
-       std::cout << fileStat.st_size << "  " << entry->d_name << std::endl;
+    
+        std::string perms = getPermissions(fileStat);
+        std::cout << perms << "  " << fileStat.st_size << "  " << entry->d_name << std::endl;
     }
-
+    
     closedir(dir);
     return 0;
+}
+
+std::string getPermissions(const struct stat &s) {
+    std::string perms = "---------";
+    if (s.st_mode & S_IRUSR) perms[0] = 'r';
+    if (s.st_mode & S_IWUSR) perms[1] = 'w';
+    if (s.st_mode & S_IXUSR) perms[2] = 'x';
+    if (s.st_mode & S_IRGRP) perms[3] = 'r';
+    if (s.st_mode & S_IWGRP) perms[4] = 'w';
+    if (s.st_mode & S_IXGRP) perms[5] = 'x';
+    if (s.st_mode & S_IROTH) perms[6] = 'r';
+    if (s.st_mode & S_IWOTH) perms[7] = 'w';
+    if (s.st_mode & S_IXOTH) perms[8] = 'x';
+    return perms;
 }
