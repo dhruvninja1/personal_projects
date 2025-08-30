04ff262fe80afaa4747a9f93b249375e3fe6f04a
Dhruv Sharma <dhruvninja@icloud.com>
2025-08-30 09:33:52 -0700

    a

--- cpp/hw.cpp
+++ cpp/hw.cpp
@@ -4,5 +4,15 @@
 using namespace std;
 
 int main(int argc, char* argv[]){
+    int a;
+    cin >> a;
+    if (a%10 >= 5){
+        a += (10-a%10);
+    }
+    else{
+        a -= a%10;
+    }
+    cout << a;
+    
 }
 
