33a1b9aa2c72b783b6cdc2c0688a502769a0b776
dhruvninja1 <dhruvninja@icloud.com>
2025-09-16 20:05:35 -0700

    ab

--- AnyoneOn/client/main.js
+++ AnyoneOn/client/main.js
@@ -1,4 +1,29 @@
-yourStatus = document.getElementById("yourStatus")
+yourStatus = document.getElementById("yourStatus");
+yourUsernameLabel = document.getElementById("yourUsernameLabel")
+userNameInputButton = document.getElementById("userNameInputButton");
+popup = document.getElementById("popup");
+overlay = document.getElementById("overlay");
+const serverUrl = "http://localhost:18080";
+
+async function updateStatus(name, status){
+    try {
+    const response = await fetch(`${serverUrl}/status`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ name, status })
+    });
+
+    if (response.ok) {
+      console.log("Status updated successfully!");
+    } else {
+      console.error("Failed to update status:", response.status);
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
 
 function updateStatusColors(){
     document.querySelectorAll(".status").forEach(el => {
@@ -18,4 +43,17 @@ menu.addEventListener("sl-select", event => {
     yourStatus.textContent = item.value;
     console.log("Cliecked");
     updateStatusColors();
+    updateStatus(username, yourStatus);
+
 });
+
+userNameInputButton.addEventListener('click', function(){
+    usernameInputField = document.getElementById("usernameInputField");
+    username = usernameInputField.value;
+    popup.style.display='none';
+    overlay.style.display='none';
+    yourUsernameLabel.textContent=username;
+    console.log(username);
+});
+
+
---
+++ AnyoneOn/host/build.sh
@@ -0,0 +1,1 @@
+g++ -std=c++17 main.cpp -I$(brew --prefix asio)/include -pthread -o myapp
